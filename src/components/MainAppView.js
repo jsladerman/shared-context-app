04bf98b4866e00c052afd3dc6bbc5a3fad123590
Nathan King <nathan@betapoint.co>
2023-06-09 22:40:27 -0400

    main section kind of styled

--- src/components/MainAppView.js
+++ src/components/MainAppView.js
@@ -2,67 +2,98 @@
 import React, { useContext } from 'react';
 import StepsContext from '../StepsContext';
 import {Utils} from '../functions/componentFunctions';
-import { Tab, Col, Nav, Row } from "react-bootstrap";
+import { Tabs, Tab, Col, Nav, Row, Container } from "react-bootstrap";
 import '../css/MainAppView.css';
+import { ArrowRightCircleFill, ChatLeftHeartFill, CompassFill, HouseDoorFill } from 'react-bootstrap-icons';
+import { SafeFill } from 'react-bootstrap-icons';
+import { CashCoin } from 'react-bootstrap-icons';
+import { WalletFill } from 'react-bootstrap-icons';
+import { BellFill } from 'react-bootstrap-icons';
+import { Safe } from 'react-bootstrap-icons';
+import { Safe2 } from 'react-bootstrap-icons';
+
+// working from this doc
+// https://www.copycat.dev/blog/bootstrap-tabs/
 
 //function
 function MainAppView() {
     const { step, setStep, userRegion } = useContext(StepsContext);
 
     //return
+    // https://react-bootstrap.netlify.app/docs/layout/grid
     return (
-        <div id="MainAppView_holder">
+        <div id="MainAppView_holder" class="MainAppView_holder">
             {step === 8 && (
-            <>
-            <div class="">
-                {/* <!-- Nav tabs --> */}
-                <div class="card">
-                    <div class="card-header">
-                    <ul class="nav nav-tabs justify-content-center" role="tablist">
-                        <li class="nav-item">
-                        <a class="nav-link active" data-toggle="tab" href="#home" role="tab">
-                            <i class="now-ui-icons objects_umbrella-13"></i> Home
-                        </a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#profile" role="tab">
-                            <i class="now-ui-icons shopping_cart-simple"></i> Profile
-                        </a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#messages" role="tab">
-                            <i class="now-ui-icons shopping_shop"></i> Messages
-                        </a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#settings" role="tab">
-                            <i class="now-ui-icons ui-2_settings-90"></i> Settings
-                        </a>
-                        </li>
-                    </ul>
-                    </div>
-                    <div class="card-body">
-                    {/* <!-- Tab panes --> */}
-                    <div class="tab-content text-center">
-                        <div class="tab-pane active" id="home" role="tabpanel">
-                        <p>I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. So when you get something that has the name Kanye West on it, it’s supposed to be pushing the furthest possibilities. I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus.</p>
+             <>
+             <Container>
+                <Row>
+                    <Col lg={11}>
+                        <Tabs defaultActiveKey="wallet">
+                        <Tab eventKey="home" title={<span><HouseDoorFill class="m-2" /> Home</span>}>
+                            Welcome
+                        </Tab>
+                        <Tab title={<span><WalletFill class="m-2" /> Wallet</span>} eventKey="wallet" >
+                            <div class="row">
+                            <div class="col-6 border bg-light" style={{opacity: .7}}>
+                            <img src='https://images.pexels.com/photos/437345/pexels-photo-437345.jpeg?auto=compress&cs=tinysrgb&w=60' />
+                            <button class="m-2 btn btn-info btn-sm" onClick={() => setStep(step + 1)}>
+                                &nbsp; Mint to ETH
+                                <Safe class="m-2" />
+                            </button>
+                            </div>
+                            <div class="col-6 border bg-light" style={{opacity: .7}}>
+                            <img src='https://images.pexels.com/photos/437345/pexels-photo-437345.jpeg?auto=compress&cs=tinysrgb&w=60' />
+                            <button class="m-2 btn btn-info btn-sm" onClick={() => setStep(step + 1)}>
+                                &nbsp; Mint to ETH
+                                <Safe class="m-2" />
+                            </button>
+                            </div>
                         </div>
-                        <div class="tab-pane" id="profile" role="tabpanel">
-                        <p> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. </p>
+                        <div class="row">
+                            <div class="col-6 border bg-light" style={{opacity: .7}}>
+
+                            <img src='https://images.pexels.com/photos/437345/pexels-photo-437345.jpeg?auto=compress&cs=tinysrgb&w=60' />
+                            <button class="m-2 btn btn-info btn-sm" onClick={() => setStep(step + 1)}>
+                                &nbsp; Mint to ETH
+                                <Safe class="m-2" />
+                            </button>
+                            </div>
+                            <div class="col-6 border bg-light" style={{opacity: .7}}>
+                            <img src='https://images.pexels.com/photos/437345/pexels-photo-437345.jpeg?auto=compress&cs=tinysrgb&w=60' />
+                            <button class="m-2 btn btn-info btn-sm" onClick={() => setStep(step + 1)}>
+                                &nbsp; Mint to ETH
+                                <Safe class="m-2" />
+                            </button>
+                            </div>
                         </div>
-                        <div class="tab-pane" id="messages" role="tabpanel">
-                        <p>I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. So when you get something that has the name Kanye West on it, it’s supposed to be pushing the furthest possibilities. I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus.</p>
+                        <div class="row">
+                            <div class="col-6 border bg-light" style={{opacity: .7}}>
+                            <img src='https://images.pexels.com/photos/437345/pexels-photo-437345.jpeg?auto=compress&cs=tinysrgb&w=60' />
+                            <button class="m-2 btn btn-info btn-sm" onClick={() => setStep(step + 1)}>
+                                &nbsp; Mint to ETH
+                                <Safe class="m-2" />
+                            </button>
+                            </div>
+                            <div class="col-6 border bg-light" style={{opacity: .7}}>
+                            <img src='https://images.pexels.com/photos/437345/pexels-photo-437345.jpeg?auto=compress&cs=tinysrgb&w=60' />
+                            <button class="m-2 btn btn-info btn-sm" onClick={() => setStep(step + 1)}>
+                                &nbsp; Mint to ETH
+                                <Safe class="m-2" />
+                            </button>
+                            </div>
                         </div>
-                        <div class="tab-pane" id="settings" role="tabpanel">
-                        <p>
-                            "I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at."
-                        </p>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </>
+                        </Tab>
+                        <Tab eventKey="navigator" title={<span>Navigator <CompassFill class="m-2" /> </span>} />
+                        <Tab eventKey="community" title={<span>Community <ChatLeftHeartFill class="m-2" /> </span>} />
+                        <Tab eventKey="updates" title={<span>Updates <BellFill class="m-2" /> </span>} />
+                        </Tabs>
+                    </Col>
+                    <Col lg={1}>
+                        <svg class="bd-placeholder-img rounded-circle" width="80" height="80" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+                    </Col>
+                </Row>
+             </Container>
+             </>
             )}
         </div>
     )
